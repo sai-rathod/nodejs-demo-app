c752a6551841ca9893d58f4cefd18bedceec1a83
Sayindra Rathod <sahilrathodrk31@gmail.com>
2025-10-17 10:57:29 +0530

    Update text in App.js for clarity

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> my first fully automated CICD new changes
+          Edit <code>src/App.js</code> my adding new line
         </p>
         <a
           className="App-link"
