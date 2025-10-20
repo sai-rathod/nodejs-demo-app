5a2f60726bea3af7369027240debf23ded24bcc6
Sayindra Rathod <sahilrathodrk31@gmail.com>
2025-10-20 12:39:45 +0530

    Fix punctuation in internship task description

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> completed first task of the internship
+          Edit <code>src/App.js</code> completed first task of the internship.
         </p>
         <a
           className="App-link"
